c7f125c4d0bff3af9501917714a27ce13dad1af4
tarush-r <tarush.r@somaiya.edu>
2024-02-16 10:20:14 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
 
+// {
+//   "code": "// alice refers to bob and bob refers to back alice!\n
+//               var alice = { name: 'Alice', follows: [] };\n
+//               var bob = { name: 'Bob', follows: [alice] };\n
+//               alice.follows.push(bob); 
+//               alice;",
+//   "sessionId": "aab9f3ba-7465-4319-820b-555b2e15433d"
+// }
+
+
+// {
+//   "root": "5e04a0ec-be54-4908-81b1-d6b076b6a4ef",
+//   "serialized": {
+//     "5e04a0ec-be54-4908-81b1-d6b076b6a4ef": {
+//       "type": "object",
+//       "value": [
+//         {
+//           "key": "302da8f6-712b-4d90-9e6c-48a549ae2e2f",
+//           "value": "d4cc776e-0626-45b1-95d5-3b4ed4ed7de7"
+//         },
+//         {
+//           "key": "a3c60cde-d339-419a-81cd-e699671e80fb",
+//           "value": "d52777dc-bc33-4e22-82a9-1de4aa2f2f18"
+//         }
+//       ]
+//     },
+//     "302da8f6-712b-4d90-9e6c-48a549ae2e2f": {
+//       "type": "string",
+//       "value": "name"
+//     },
+//     "d4cc776e-0626-45b1-95d5-3b4ed4ed7de7": {
+//       "type": "string",
+//       "value": "Alice"
+//     },
+//     "a3c60cde-d339-419a-81cd-e699671e80fb": {
+//       "type": "string",
+//       "value": "follows"
+//     },
+//     "d52777dc-bc33-4e22-82a9-1de4aa2f2f18": {
+//       "type": "array",
+//       "value": [
+//         "86f88625-035c-4e3c-8374-27c2ec41bf67"
+//       ]
+//     },
+//     "86f88625-035c-4e3c-8374-27c2ec41bf67": {
+//       "type": "object",
+//       "value": [
+//         {
+//           "key": "302da8f6-712b-4d90-9e6c-48a549ae2e2f",
+//           "value": "64e38be1-437a-4505-b183-3f34b8b474b3"
+//         },
+//         {
+//           "key": "a3c60cde-d339-419a-81cd-e699671e80fb",
+//           "value": "32b7f10e-66ed-46b0-b29c-1c604ea2f0d8"
+//         }
+//       ]
+//     },
+//     "64e38be1-437a-4505-b183-3f34b8b474b3": {
+//       "type": "string",
+//       "value": "Bob"
+//     },
+//     "32b7f10e-66ed-46b0-b29c-1c604ea2f0d8": {
+//       "type": "array",
+//       "value": [
+//         "5e04a0ec-be54-4908-81b1-d6b076b6a4ef"
+//       ]
+//     }
+//   }
+// }
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
