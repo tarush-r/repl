fce1e060507c6275d747a37343a113df81020b04
tarush-r <tarush.r@somaiya.edu>
2024-02-16 14:33:34 -0800

    everything works
    Signed-off-by: tarush-r <tarush.r@somaiya.edu>

--- src/state/store.js
+++ src/state/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import replReducer from './repl/replSlice'
-
+import replReducer from "./repl/replSlice";
 
 export const store = configureStore({
-    reducer: {
-        repl: replReducer 
-    },
+  reducer: {
+    repl: replReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,11 +2,9 @@ import Data from "../models/Data";
 import { stringify } from "flatted";
 
 const getKey = (data, key) => {
-  console.log("getting key for ", key);
   var keyType = data[key]["type"];
   var keyValue = data[key]["value"];
   if (keyType == "string") {
-    console.log("returned ", keyValue);
     return keyValue;
   } else if (keyType == "Number") {
     return keyValue;
@@ -16,9 +14,6 @@ const getKey = (data, key) => {
 
 const convertToObj = (data, key, seen) => {
   let curr = {};
-  //   if (seen[key]) {
-  //     return seen[key];
-  //   }
   var node = data[key];
   var nodeType = node["type"];
   var nodeVal = node["value"];
@@ -30,36 +25,24 @@ const convertToObj = (data, key, seen) => {
 
   console.log(nodeType, " ", nodeVal);
   if (nodeType != "object" && nodeType != "array") {
-    // var kVal,
-    //     kType = getKey(data[k]);
     return nodeVal;
   }
   if (nodeType == "object") {
-    // var temp = {}
     nodeVal.forEach((item) => {
       var k = item["key"];
       var v = item["value"];
       var kVal = getKey(data, k);
       console.log("printing; ", kVal);
       curr[kVal] = convertToObj(data, v, seen);
-      //   curr.push(convertToObj(data, item, seen, curr));
     });
-    // for (let [k, v] of nodeVal.entries()) {
-    //   var kVal,
-    //     kType = getKey(data[k]);
-    //     console.log(kVal, ' for node with key ', key);
-    //   curr[kVal] = convertToObj(data, v, seen, curr);
-    // }
     console.log(curr);
   }
   if (nodeType == "array") {
-    // var temp = {}
     curr = [];
     nodeVal.forEach((item) => {
       curr.push(convertToObj(data, item, seen));
     });
   }
-  //   seen[key] = curr;
   return curr;
 };
 
